3d6b9d70d0bcd0072628e926ef7fab28e2a197f3
kykykyl2 <kyllian.marquet@gmail.com>
2024-06-05 16:45:59 +0200

    add outline none input

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -4,4 +4,12 @@ const Title = ({ title }) => {
   return(<h1 className='title'>{title}</h1>)
 } 
 
+Title.propTypes = {
+  title: String
+}
+
+Title.defaultProps = {
+  title: ''
+}
+
 export default Title
