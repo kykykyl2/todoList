46309854bffc79f24c0b0ea2f9dcc69ed00b631e
kykykyl2 <kyllian.marquet@gmail.com>
2024-06-06 16:38:23 +0200

    update

---
+++ src/components/List/index.jsx
@@ -0,0 +1,39 @@
+import './styles.css'
+import ListItem from '../ListItem'
+
+const List = () => (
+    <div className='list'>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+    </div>
+)
+
+export default List 
---
+++ src/components/ListItem/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+const ListeItem = () => (
+    <div className='liste_item'>
+        <h3>titre</h3>
+        <input type="checkbox" />
+    </div>
+    
+)
+
+export default ListeItem
