2d7a4bd133e7365c65ebce3c561d9ab1b3f8b738
kykykyl2 <kyllian.marquet@gmail.com>
2024-06-07 11:33:25 +0200

    update

---
+++ src/components/ToDoSend/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+import Button from '../Button'
+import Input from '../Input'
+
+function ToDoSend () {
+    return(<form className='todosend' >
+    <Input />
+    <Button />
+    </form>)
+}
+
+export default ToDoSend
